e32630133d7808e60d869e74757c041139bf9363
Hitesh Walia <81966053+Hiteshwalia4@users.noreply.github.com>
2024-09-22 11:42:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template,request
+import pegasus_samsum_model
+import tokenizer
+from transformers import pipeline
+from transformers import AutoTokenizer
+
+app = Flask(__name__)
+
+tokenizer = AutoTokenizer.from_pretrained("tokenizer/")
+
+@app.route("/")
+def msg():
+    return render_template('index.html')
+
+@app.route("/summarize",methods=['POST','GET'])
+def getSummary():
+    reference = request.form['data']
+    length = int(request.form['len'])
+    gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": length}
+    pipe1 = pipeline("summarization", model="pegasus_samsum_model",tokenizer=tokenizer)
+    result = pipe1(reference, **gen_kwargs)[0]["summary_text"]
+    return render_template('summary.html',reference=reference, result=result)
+
+if __name__ =="__main__":
+    app.run(debug=True,port=8000)
+
+
